f7e35b70d8d063492d466ac7e078bd5d898a332b
credsuk <credsuk@gmail.com>
2016-02-12 13:52:27 +0900

    Signed-off-by: credsuk <credsuk@gmail.com>

--- src/dto/message/MessageDataBean.java
+++ src/dto/message/MessageDataBean.java
@@ -1,5 +1,58 @@
 package dto.message;
 
-public class MessageDataBean {
+import java.sql.Timestamp;
 
+public class MessageDataBean {
+	private int MESSAGE_NUM;		// 메시지 글 번호
+	private String EMAIL;			// 받는사람 메일
+	private String TITLE;			// 제목
+	private String CONTENT;			// 내용
+	private String TAG;				// 보낸사람 정보
+	private int READ_YN;			// 읽은 유,무 확인용
+	private Timestamp REG_DATE;		// 보낸날자
+	
+	public int getMESSAGE_NUM() {
+		return MESSAGE_NUM;
+	}
+	public void setMESSAGE_NUM(int mESSAGE_NUM) {
+		MESSAGE_NUM = mESSAGE_NUM;
+	}
+	public String getEMAIL() {
+		return EMAIL;
+	}
+	public void setEMAIL(String eMAIL) {
+		EMAIL = eMAIL;
+	}
+	public String getTITLE() {
+		return TITLE;
+	}
+	public void setTITLE(String tITLE) {
+		TITLE = tITLE;
+	}
+	public String getCONTENT() {
+		return CONTENT;
+	}
+	public void setCONTENT(String cONTENT) {
+		CONTENT = cONTENT;
+	}
+	public String getTAG() {
+		return TAG;
+	}
+	public void setTAG(String tAG) {
+		TAG = tAG;
+	}
+	public int getREAD_YN() {
+		return READ_YN;
+	}
+	public void setREAD_YN(int rEAD_YN) {
+		READ_YN = rEAD_YN;
+	}
+	public Timestamp getREG_DATE() {
+		return REG_DATE;
+	}
+	public void setREG_DATE(Timestamp rEG_DATE) {
+		REG_DATE = rEG_DATE;
+	}
+	
+	
 }
--- src/handler/message/MessageFormHandler.java
+++ src/handler/message/MessageFormHandler.java
@@ -3,7 +3,6 @@ package handler.message;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,34 +10,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import dto.message.MessageDataBean;
 import handler.Commandhandler;
 
 @Controller
-public class MessageProHandler implements Commandhandler {
+public class MessageFormHandler implements Commandhandler{
 	
+	@RequestMapping("/messageForm")
 	@Override
 	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		Map<String, Object> map = new HashMap<String, Object>();
-		// 한글이 넘어오니 처리해줌
-		request.setCharacterEncoding("utf-8");
-		
-		// 바구니 객체 생성
-		MessageDataBean dto = new MessageDataBean();
-		
-		// 바구니에 자료 담기
-		dto.setEMAIL( request.getParameter("recipient") );
-		dto.setTITLE( request.getParameter("title") );
-		dto.setCONTENT( request.getParameter("content") );
-		dto.setTAG( );
-		dto.setREAD_YN(rEAD_YN);
-		dto.setREG_DATE(rEG_DATE);
 		
 		
 		
-		
-		
-		map.put("page", "/FJ_MESSAGE/messagePro");
+		map.put("page", "/FJ_MESSAGE/messageForm");
 		return new ModelAndView("/FJ_MAIN/main", map);
 	}
 
