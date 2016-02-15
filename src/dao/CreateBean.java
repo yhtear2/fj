eb50bcf3cde13b2f094aaca6b924c57b373c83f0
송윤호 <yhtear2@naver.com>
2016-02-15 12:46:47 +0900

    Signed-off-by: 송윤호 <yhtear2@naver.com>

--- src/dao/CreateBean.java
+++ src/dao/CreateBean.java
@@ -5,12 +5,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
+import dao.admin.AdminDBBean;
+import dao.admin.AdminDao;
+import dao.board.BoardDBBean;
+import dao.board.BoardDao;
 import dao.comp.CompDBBean;
 import dao.comp.CompDao;
+import dao.join.JoinDBBean;
+import dao.join.JoinDao;
 import dao.message.MessageDBBean;
+import dao.recruit.RecruitDBBean;
+import dao.user.UserDBBean;
 
 @Configuration
 public class CreateBean {
+	
+	@Bean
+	public AdminDao adminDao(){
+		return new AdminDBBean();
+	}
+	
+	@Bean
+	public BoardDao boardDao(){
+		return new BoardDBBean();
+	}
 	
 	@Bean
 	public CompDao compDao() {
@@ -18,8 +36,23 @@ public class CreateBean {
 	}
 	
 	@Bean
+	public JoinDao joinDao() {
+		return new JoinDBBean();
+	}
+	
+	@Bean
 	public MessageDBBean messageDao() {
 		return new MessageDBBean();
+	}
+	
+	@Bean
+	public RecruitDBBean recruitDao() {
+		return new RecruitDBBean();
+	}
+	
+	@Bean
+	public UserDBBean userDao(){
+		return new UserDBBean();
 	}
 	
 	@Bean
