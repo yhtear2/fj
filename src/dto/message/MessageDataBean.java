8102da50c41d60208c4c139e87087f068f37c358
credsuk <credsuk@gmail.com>
2016-02-15 12:33:10 +0900

    Signed-off-by: credsuk <credsuk@gmail.com>

--- src/dto/message/MessageDataBean.java
+++ src/dto/message/MessageDataBean.java
@@ -3,55 +3,55 @@ package dto.message;
 import java.sql.Timestamp;
 
 public class MessageDataBean {
-	private int MESSAGE_NUM;		// 메시지 글 번호
-	private String EMAIL;			// 받는사람 메일
-	private String TITLE;			// 제목
-	private String CONTENT;			// 내용
-	private String TAG;				// 보낸사람 정보
-	private int READ_YN;			// 읽은 유,무 확인용
-	private Timestamp REG_DATE;		// 보낸날자
+	private int message_num;		// 메시지 글 번호
+	private String email;			// 받는사람 메일
+	private String title;			// 제목
+	private String content;			// 내용
+	private String tag;				// 보낸사람 정보
+	private int read_yn;			// 읽은 유,무 확인용
+	private Timestamp reg_date;		// 보낸날자
 	
-	public int getMESSAGE_NUM() {
-		return MESSAGE_NUM;
+	public int getMessage_num() {
+		return message_num;
 	}
-	public void setMESSAGE_NUM(int mESSAGE_NUM) {
-		MESSAGE_NUM = mESSAGE_NUM;
+	public void setMessage_num(int message_num) {
+		this.message_num = message_num;
 	}
-	public String getEMAIL() {
-		return EMAIL;
+	public String getEmail() {
+		return email;
 	}
-	public void setEMAIL(String eMAIL) {
-		EMAIL = eMAIL;
+	public void setEmail(String email) {
+		this.email = email;
 	}
-	public String getTITLE() {
-		return TITLE;
+	public String getTitle() {
+		return title;
 	}
-	public void setTITLE(String tITLE) {
-		TITLE = tITLE;
+	public void setTitle(String title) {
+		this.title = title;
 	}
-	public String getCONTENT() {
-		return CONTENT;
+	public String getContent() {
+		return content;
 	}
-	public void setCONTENT(String cONTENT) {
-		CONTENT = cONTENT;
+	public void setContent(String content) {
+		this.content = content;
 	}
-	public String getTAG() {
-		return TAG;
+	public String getTag() {
+		return tag;
 	}
-	public void setTAG(String tAG) {
-		TAG = tAG;
+	public void setTag(String tag) {
+		this.tag = tag;
 	}
-	public int getREAD_YN() {
-		return READ_YN;
+	public int getRead_yn() {
+		return read_yn;
 	}
-	public void setREAD_YN(int rEAD_YN) {
-		READ_YN = rEAD_YN;
+	public void setRead_yn(int read_yn) {
+		this.read_yn = read_yn;
 	}
-	public Timestamp getREG_DATE() {
-		return REG_DATE;
+	public Timestamp getReg_date() {
+		return reg_date;
 	}
-	public void setREG_DATE(Timestamp rEG_DATE) {
-		REG_DATE = rEG_DATE;
+	public void setReg_date(Timestamp reg_date) {
+		this.reg_date = reg_date;
 	}
 	
 	
