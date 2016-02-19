0643cfdeef32cdfd77c4ced09f0439abfda5c6e3
송윤호 <yhtear2@naver.com>
2016-02-19 14:27:00 +0900

    Signed-off-by: 송윤호 <yhtear2@naver.com>

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -1,13 +1,19 @@
 /* name 		: focusAlert(msg, obj)
  * parameter 	: msg, obj
- * 	msg : 출력할 메세지
- * 	obj : 출력 후 포커스 할 오브젝트
+ * - msg 		: 출력할 메세지
+ * - obj 		: 출력 후 포커스 할 오브젝트
+ * description 	: 메세지 출력 후 특정 오브젝트에 포커스 부여
  */
 function focusAlert(msg, obj){
 	alert(msg);
 	obj.focus();
 }
 
+/* name 		: errorAlert(msg)
+ * parameter 	: msg
+ * - msg 		: 출력할 메세지
+ * description 	: 메세지 출력 후 페이지 뒤로가기 
+ */
 function errorAlert(msg){
 	alert(msg);
 	history.back();
