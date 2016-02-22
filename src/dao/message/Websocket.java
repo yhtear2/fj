359c88d1ab44814d25dcfa6ef12bda677d4b0939
credsuk <credsuk@gmail.com>
2016-02-22 14:52:16 +0900

    Signed-off-by: credsuk <credsuk@gmail.com>

--- src/dao/message/Websocket.java
+++ src/dao/message/Websocket.java
@@ -2,9 +2,10 @@ package dao.message;
 
 
 import java.io.IOException;
-import java.util.*;
-
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -13,40 +14,34 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/broadcasting")
+@ServerEndpoint(value = "/websocket/chat") //클라이언트가 접속할 때 사용될 URI
 public class Websocket {
-	private static final Map<String,Session> sessionMap = new HashMap<String,Session>();
-	    
-	// 클라이언트가 새로 접속할 때마다 한개의 Session 객체가 생성된다.
+	// Map<유저memId, 세션> 저장할 공간 마련
+    private static final Map<String,Session> sessionMap = new HashMap<String,Session>();
+    // 유저memId가 저장될 공간
+    private String memId;
+    // 클라이언트가 새로 접속할 때마다 한개의 Session 객체가 생성된다.
     // Session 객체를 컬렉션에 보관하여 두고 해당 클라이언트에게 데이터를 전송할 때마다 사용한다
     private Session session;
-    private String nickname;
 
     @OnOpen
     public void start(Session session) {
     	System.out.println("클라이언트 접속됨 : "+session);
     	// 접속자마다 한개의 세션이 생성되어 데이터 통신수단으로 사용됨
         this.session = session;
-        System.out.println("이히힛 뭐지?? " +session.getPathParameters().get("memId"));
-        System.out.println("이히힛 뭐111지?? " +session.getUserProperties().get("memID").toString());
-
-
-
+        //nickname = session.getRequestParameterMap().get("id").get(0);
+        memId = session.getRequestURI().toString().substring(25); 
+        System.out.println("User MemId : " + memId );
         
-        
-        
-        //nickname = session.getRequestParameterMap().get("memId").get(0);
-        
-        System.out.println("nickname : " + nickname);
-        sessionMap.put("nickname", session);
+        sessionMap.put(memId, session);
+        String message = String.format("* %s %s", memId, "has joined.");
+        broadcast(message);
     }
-   
-
 
     @OnClose
     public void end() {
         sessionMap.remove(this.session);
-        String message = String.format("* %s %s", nickname, "has disconnected.");
+        String message = String.format("* %s %s", memId, "has disconnected.");
         broadcast(message);
     }
 
@@ -55,12 +50,12 @@ public class Websocket {
     public void incoming(String message) {
     	
     	String threadName = "Thread-Name:"+Thread.currentThread().getName();
-    	System.out.println("메시지 도착:"+threadName+", "+nickname);
+    	System.out.println("메시지 도착:"+threadName+", "+memId);
         if(message==null || message.trim().equals("")) return;
-        String filteredMessage = String.format("%s: %s", nickname, message);
+        String filteredMessage = String.format("%s: %s", memId, message);
         
         //Guest0의 메시지는 특정 클라이언트(Guest2)에게만 전달하는 경우
-        if(this.nickname.equals("Guest0")) {
+        if(this.memId.equals("Guest0")) {
         	sendToOne(filteredMessage, sessionMap.get("Guest2"));
         }
         else //현재 접속된 모든 클라이언트에게 메시지를 전달하는 경우
@@ -71,8 +66,8 @@ public class Websocket {
 
     @OnError
     public void onError(Throwable t) throws Throwable {
-        System.err.println("오류/세션제거("+nickname+"):Chat Error: " + t.toString());
-        sessionMap.remove(this.nickname);
+        System.err.println("오류/세션제거("+memId+"):Chat Error: " + t.toString());
+        sessionMap.remove(this.memId);
     }
 
     // 클라이언트로부터 도착한 메시지를 특정 수신자(Session)에게만 전달한다
