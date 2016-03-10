b022f1fe7d0a705ff6ba032efd7e09c19e80ef07
고병완 <user1@KH1>
2016-03-10 09:34:12 +0900

    1차 DB 수정
    Signed-off-by: 고병완 <user1@KH1>

--- src/dto/board/BoardCommentDataBean.java
+++ src/dto/board/BoardCommentDataBean.java
@@ -6,19 +6,19 @@ public class BoardCommentDataBean {
  
 	private String email;       // 이메일 
 	private String recontent;   // 댓글내용 
-	private int mom_board_num;  // 부모 게시판 id (부모글 번호)
+//	private int mom_board_num;  // 부모 게시판 id (부모글 번호)
 	private int board_num;      // 게시판 id (글번호)
 	private int recom_count;    // 좋아요 수
 	private int re_step;        // 게시글의 구분
 	private Timestamp reg_date; // 작성날짜 
 	
-	
+	/*
 	public int getMom_board_num() {
 		return mom_board_num;
 	}  
 	public void setMom_board_num(int mom_board_num) {
 		this.mom_board_num = mom_board_num;
-	}
+	}  */
 	public String getEmail() {
 		return email;
 	}
