3c06730ae9c20dfefedba73494c93c7058711f2c
허필정 <hp8706@gmail.com>
2016-03-14 15:17:46 +0900

    채용공고 유효성검사 하는 중입니다. - (의외의 난관)
    Signed-off-by: 허필정 <hp8706@gmail.com>

--- WebContent/FJ_RECRUIT/recruit.js
+++ WebContent/FJ_RECRUIT/recruit.js
@@ -47,7 +47,63 @@ function checkCheckbox1(frm,obj,num) {
 // recruitWriteForm에서 섬잇을 누르면 실행되는 메소드
 function recruitwritecheck(){
 	// 일단 예외처리는 안할래....귀.찮....;;
+	/*
+	var chtitle = document.recruitWriteForm.title.value;
+	var chwork_area = document.getElementsByName("work_area");
+	var charea_true = ""; 
+	for(var i=0; i<chwork_area.length ; i++){
+		if(chwork_area[i].checked == true){  
+			chwork_true += chwork_area[i].value;   
+		}
+	}
 	
+	var chmin_salary = document.recruitWriteForm.min_salary.value;
+	var chmax_salary = document.recruitWriteForm.max_salary.value;
+	var chwork_type = document.getElementsByName("work_type");
+	var chtype_true = "";
+	for(var i=0; i<chwork_type.length ; i++){
+		if(chwork_type[i].checked == true){  
+			chtype_true += chwork_type[i].value;   
+		}
+	}
+	var chwork_day = document.recruitWriteForm.work_day.value;
+	var chedu = document.recruitWriteForm.edu.value;
+	var chgender = document.recruitWriteForm.gender.value;
+	var chmin_age = document.recruitWriteForm.min_age.value;
+	var chmax_age = document.recruitWriteForm.max_age.value;
+	var chage = document.recruitWriteForm.age.value;
+	var chcareer = document.recruitWriteForm.career.value;
+	var chmin_career = document.recruitWriteForm.min_career.value;
+	var chmax_career = document.recruitWriteForm.max_career.value;
+	var chbusiness_part = document.recruitWriteForm.business_part.value;
+	var chskill = document.recruitWriteForm.skill.value;
+	var chpeople_count = document.recruitWriteForm.people_count.value;
+	var chstart_date = document.recruitWriteForm.start_date.value;
+	var chend_date = document.recruitWriteForm.end_date.value;
+	var chcontent = $('input[name=content]').val( $('#summernote').summernote('code') );
+	
+	if( chtitle == "" ) {
+		alert("제목을 입력해주세요");
+		return false;
+	} else if( charea_true == "") {
+		alert("근무지역을 선택해주세요");
+		return false;
+	} else if( chmin_salary.substr(0,3) > chmax_salary.substr(0,3) ) {
+		alert("연봉이 잘 못 선택되었습니다");
+		return false;
+	} else if( chtype_true == "") {
+		alert("근무형태를 선택해주세요");
+		return false;
+	} else if( chwork_day == "") {
+		alert("근무요일을 선택해주세요");
+		return false;
+	} else if( chedu == "") {
+		alert("학력을 선택해주세요");
+		return false;
+	} else if( chgender == "") {
+		alert("성별을 선택해주세요");
+		return false;
+	}*/
 	// 콘텐츠 내용에 쓴거 넘기기 위해서 이렇게~
 	$('input[name=content]').val( $('#summernote').summernote('code') );
 }
@@ -244,5 +300,5 @@ $(document).ready(function() {
 
 	
 });
-
+//채용공고 유효성 검사
 
